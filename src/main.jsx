0644eb0dedfaae1672b4b02f6220cd1f07222ac2
Jhimi Talukder <talukderjhimi@gmail.com>
2023-07-29 15:03:16 +0600

    modidy

--- src/main.jsx
+++ src/main.jsx
@@ -1,60 +1,68 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import MainLayout from './MainLayout/MainLayout.jsx';
-import Home from './pages/Home/Home.jsx';
-import Users from './pages/Users/Users.jsx';
-import Login from './pages/Login/Login.jsx';
-import SignUp from './pages/SignUp/SignUp.jsx';
-import ErrorPage from './pages/ErrorPage/ErrorPage.jsx';
+import MainLayout from "./MainLayout/MainLayout.jsx";
+import Home from "./pages/Home/Home.jsx";
+import Users from "./pages/Users/Users.jsx";
+import Login from "./pages/Login/Login.jsx";
+import SignUp from "./pages/SignUp/SignUp.jsx";
+import ErrorPage from "./pages/ErrorPage/ErrorPage.jsx";
 import {
   useQuery,
   useMutation,
   useQueryClient,
   QueryClient,
   QueryClientProvider,
-} from '@tanstack/react-query'
-import AuthProvider from './Provider/AuthProvider.jsx';
-import AllUsers from './pages/Users/AllUsers.jsx';
-import PrivateRoute from './Routes/PrivateRoute';
-import ProfilePage from './pages/ProfilePage/ProfilePage';
+} from "@tanstack/react-query";
+import AuthProvider from "./Provider/AuthProvider.jsx";
+import AllUsers from "./pages/Users/AllUsers.jsx";
+import PrivateRoute from "./Routes/PrivateRoute";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
 
 // Create a client
-const queryClient = new QueryClient()
-
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout></MainLayout>,
     children: [
-      {path: '/' , element: <PrivateRoute><Home></Home></PrivateRoute>},
-      {path: '/user/users' , element: <Users></Users>,
-    children:[
-      {path:'/user/users', element: <AllUsers></AllUsers>}
-    ]},
-      {path: '/media' , element: <p>media</p>},
-      {path: '/profilepage/:email' , element: <ProfilePage></ProfilePage>,loader: ({ params }) => fetch(params.email),},
-      {path: '/play' , element: <p>play</p>},
-      {path:'/login' , element: <Login></Login>},
-      {path:'/signup' , element: <SignUp></SignUp>},
-      {path: '*', element: <ErrorPage></ErrorPage>},
-    
-    ]
-  }
- 
+      {
+        path: "/",
+        element: (
+            <Home></Home>
+        ),
+      },
+      {
+        path: "/user/users",
+        element: <Users></Users>,
+        children: [{ path: "/user/users", element: <AllUsers></AllUsers> }],
+      },
+      { path: "/media", element: <p>media</p> },
+      {
+        path: "/profilepage/:email",
+        element: (
+          <PrivateRoute>
+            <ProfilePage></ProfilePage>
+          </PrivateRoute>
+        ),
+        loader: ({ params }) => fetch(params.email),
+      },
+      { path: "/play", element: <p>play</p> },
+      { path: "/login", element: <Login></Login> },
+      { path: "/signup", element: <SignUp></SignUp> },
+      { path: "*", element: <ErrorPage></ErrorPage> },
+    ],
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  <AuthProvider>
-  <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
-  </AuthProvider>
-  </React.StrictMode>,
-)
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </AuthProvider>
+  </React.StrictMode>
+);
