3cdf84370c1d0f5778744ed623cd8bb47385ebc3
Jhimi Talukder <talukderjhimi@gmail.com>
2023-07-31 17:24:02 +0600

    modify

--- src/component/Icons/Icons.jsx
+++ src/component/Icons/Icons.jsx
@@ -5,7 +5,7 @@ import './Icon.css'
 const Icons = () => {
   return (
     // <div className="flex overflow-x-auto cart text-sm h-full mb-1">
-    <div className="md:w-[20%] overflow-auto h-[100vh] cart md:sticky md:top-0"> 
+    <div className="md:w-[20%] mb-5 md:mb-0 overflow-auto h-[100vh] cart md:sticky md:top-0"> 
       {categories.map((item) => (
         <CategoryBox
           key={item.label}
