24a712d996ae37b6959026b2a12326bdf7a26a30
jhimi01 <talukderjhimi@gmail.com>
2025-01-08 18:49:08 +0600

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
 export default {
   content: [
     "./index.html",
@@ -7,8 +9,8 @@ export default {
   theme: {
     extend: {},
   },
+  plugins: [daisyui],
   daisyui: {
-    themes: ["light", "dark", "cupcake", "emerald",],
+    themes: ["light", "dark", "cupcake"],
   },
-  plugins: [require("daisyui")],
-}
+};
